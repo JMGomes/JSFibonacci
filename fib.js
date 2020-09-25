3db013f3bcd5afecfe467210bc2c8774081463e4
João Manuel Gomes <33399876+JMGomes@users.noreply.github.com>
2020-09-25 18:18:43 +0100

    Update fib.js

--- fib.js
+++ fib.js
@@ -18,11 +18,11 @@ const impls = [
 assertResults(impls);
 
 // inputs to compare
-const inputsTo50 = [ 10, 20, 30, 40];
+const inputsTo40 = [ 10, 20, 30, 40];
 const inputsTo15k = [ 50, 100, 150, 200, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 5000, 7500, 10000, 12500, 15000];
 const inputsTo10M = [ 1000, 1500, 2000, 2500, 5000, 7500, 10000, 15000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 100000, 200000, 300000, 500000, 1000000, 5000000, 1000000];
 
-resultToCSV('inputsTo50.csv', calcFibonacci(impls, inputsTo50));
+resultToCSV('inputsTo40.csv', calcFibonacci(impls, inputsTo40));
 
 // remove recursive impl and increase input
 impls.shift();
